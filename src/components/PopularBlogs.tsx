98d2fbf6aba6eb0384f75b24ca83fe7283c0d266
SobhieSaad <sobhie.saad1@gmail.com>
2025-08-10 22:11:15 +0200

    build react typescript store

---
+++ src/components/PopularBlogs.tsx
@@ -0,0 +1,75 @@
+import { MessageCircle, ThumbsUp } from 'lucide-react'
+import React from 'react'
+
+const PopularBlogs = () => {
+
+    const blogs = [
+        {
+            title: "my blog",
+            author: "Sobhie",
+            like: 42,
+            comments: 1
+        },
+        {
+            title: "my blog 2",
+            author: "Saad",
+            like: 50,
+            comments: 3
+        },
+        {
+            title: "last blog",
+            author: "author",
+            like: 200,
+            comments: 10
+        },
+    ]
+    return (
+        <div
+
+            className='bg-white p-5 w-[23rem] mt-4 border ml-5 rounded'
+        >
+
+            <h2 className="text-xl font-bold mb-5">
+                Popular Blogs
+            </h2>
+            <ul>
+                {blogs.map((blog, index) => (
+                    <li
+                        key={index}
+                        className='mb-4'
+                    >
+                        <div className='flex justify-between items-center'>
+                            <span
+                                className='font-bold mb-2'
+                            >
+                                {blog.title}
+                            </span>
+                        </div>
+
+                        <span
+                            className='text-gray-600'
+                        >
+                            Publish by {blog.author}
+                        </span>
+
+                        <div
+                            className='flex items-center mt-2'
+                        >
+                            <MessageCircle size={16} />
+                            <span className='text-gray-500 mr-5 ml-1'>
+                                {blog.like}
+                            </span>
+
+                            <ThumbsUp size={16} />
+                            <span className='text-ray-500 mr-2 ml-2'>
+                                {blog.comments}
+                            </span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default PopularBlogs
